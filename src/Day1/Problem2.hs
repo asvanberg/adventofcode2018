69acffc7fa3d416c4fd494501bcb48f0e72278eb
Andreas Svanberg <andreas.svanberg@mensa.se>
2018-12-05 23:39:46 +0100

    Simplified day 1

--- src/Day1/Problem2.hs
+++ src/Day1/Problem2.hs
@@ -1,25 +1,19 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Day1.Problem2 where
 
-import Control.Applicative (Alternative)
-import Control.Monad.IO.Class
 import Control.Monad.State
-import Data.Bifunctor
 import Data.Set (Set)
 import qualified Data.Set as Set
-import System.IO hiding (isEOF)
 
-import Day1.Problem1 (isEOF, readNumber)
+import Day1.Problem1 (frequencyChanges)
 
 problem2 :: IO ()
 problem2 = do
-  h <- openFile "src/Day1/input" ReadMode
-  deltas <- frequencyChanges h
+  deltas <- frequencyChanges "src/Day1/input"
   f <- evalStateT (duplicateFrequency (cycle deltas)) (Set.empty, 0)
-  hClose h
   print f
 
-duplicateFrequency :: (MonadState (Set Int, Int) m, MonadIO m) => [Int] -> m Int
+duplicateFrequency :: (MonadState (Set Int, Int) m) => [Int] -> m Int
 duplicateFrequency (delta:deltas) = do
   (seen, current) <- get
   let newFrequency = current + delta
@@ -28,14 +22,3 @@ duplicateFrequency (delta:deltas) = do
     else do
       put (Set.insert newFrequency seen, newFrequency)
       duplicateFrequency deltas
-
-frequencyChanges :: (MonadIO m) => Handle -> m [Int]
-frequencyChanges handle = loop []
-  where
-    loop l = do
-      eof <- isEOF handle
-      if eof
-        then return $ reverse l
-        else do
-          number <- readNumber handle
-          loop (number : l)
