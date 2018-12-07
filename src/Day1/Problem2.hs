c2edafad69b7f886b6772f31439c367ae7345249
Andreas Svanberg <andreas.svanberg@mensa.se>
2018-12-07 02:53:56 +0100

    Some alternative solutions to day 1 problem 2

--- src/Day1/Problem2.hs
+++ src/Day1/Problem2.hs
@@ -6,19 +6,34 @@ import Control.Monad.State
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-import Day1.Problem1 (frequencyChanges)
+import Day1.Problem1 (frequencyChanges, iif)
 
 solution :: (MonadIO m) => m Int
 solution = do
   deltas <- frequencyChanges "src/Day1/input"
-  evalStateT (duplicateFrequency (cycle deltas)) (Set.empty, 0)
+  --evalStateT (duplicateFrequency (cycle deltas) 0) Set.empty
+  return $ duplicateFrequency3 (cycle deltas) Set.empty 0
 
-duplicateFrequency :: (MonadState (Set Int, Int) m) => [Int] -> m Int
-duplicateFrequency (delta:deltas) = do
-  (seen, current) <- get
+duplicateFrequency :: (MonadState (Set Int) m) => [Int] -> Int -> m Int
+duplicateFrequency (delta:deltas) current = do
+  seen <- get
   let newFrequency = current + delta
   if Set.member newFrequency seen
     then return newFrequency
     else do
-      put (Set.insert newFrequency seen, newFrequency)
-      duplicateFrequency deltas
+      modify (Set.insert newFrequency)
+      duplicateFrequency deltas newFrequency
+
+duplicateFrequency2 :: (MonadState (Set Int) m) => [Int] -> Int -> m Int
+duplicateFrequency2 (delta:deltas) current = do
+  let newFrequency = current + delta
+  Set.member newFrequency <$> get >>= iif
+    (return newFrequency)
+    (modify (Set.insert newFrequency) *> duplicateFrequency2 deltas newFrequency)
+
+duplicateFrequency3 :: [Int] -> Set Int -> Int -> Int
+duplicateFrequency3 (delta:deltas) seen current =
+  let newFrequency = current + delta
+  in if Set.member newFrequency seen
+    then newFrequency
+    else duplicateFrequency3 deltas (Set.insert newFrequency seen) newFrequency
